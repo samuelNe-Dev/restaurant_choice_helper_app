81f149e7632c8d503b1e9f2ba2bac037f09f6d7d
samuelNe-Dev <negash.samuel@yahoo.de>
2021-08-08 17:08:14 +0200

    Init. Project

---
+++ android/app/src/main/kotlin/com/example/restaurant_choice_helper_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.restaurant_choice_helper_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/provider/auth_provider.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider{
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  // Create user account
+  
+
+
+  // Sign in with user account
+
+
+
+  // Sign out with user account
+
+
+
+  // Reset Password
+
+
+}
